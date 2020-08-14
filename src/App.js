551bc896d357eeba45b7a490db38c2cd474786c3
danko.anton <danko.anton@gmail.com>
2020-08-14 22:05:00 +0300

    added total balance handler function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Total from './components/total/Total';
 import History from './components/history/History';
 import Operation from './components/operation/Operation';
 
-function App() {
-  return (
-    <>
-      <header>
-        <h1>Кошелек</h1>
-        <h2>Калькулятор расходов</h2>
-    	</header>
+class App extends Component {
 
-			<main>
-        <div className="container">
-            <Total />
-						<History />
-						<Operation />            
-				</div>
-			</main>
-		</>
-  );
+	state = {
+		transactions: [],
+		description: '',
+		amount: '',
+		resultIncome: 0,
+		resultExpenses: 0,
+		totalBalance: 0,
+	}
+
+	addTransaction = (add) => {
+		const transactions = [...this.state.transactions,
+			{
+				id: `cmr${(+new Date()).toString(16)}`,
+				description: this.state.description,
+				amount: parseFloat(this.state.amount),
+				add
+			}
+		];
+
+		this.setState({ 
+			transactions,
+			description: '',
+			amount: '',	
+		}, this.getTotalBalance);
+	}
+
+	addAmount = (e) => {
+		this.setState({amount: e.target.value});
+	}
+	addDescription = (e) => {
+		this.setState({description: e.target.value});
+	}
+
+	getIncome = () => this.state.transactions
+		.reduce((acc, item) => item.add ? item.amount + acc : acc, 0)
+	
+
+	getExpenses = () => this.state.transactions
+		.reduce((acc, item) => !item.add ? item.amount + acc : acc, 0)
+
+	getTotalBalance() {
+		const resultIncome = this.getIncome();
+		const resultExpenses = this.getExpenses();
+		
+		const totalBalance = resultIncome - resultExpenses;
+
+		this.setState({resultIncome, resultExpenses, totalBalance});
+	}
+
+	render() {
+		return (
+			<>
+				<header>
+					<h1>Кошелек</h1>
+					<h2>Калькулятор расходов</h2>
+				</header>
+
+				<main>
+					<div className="container">
+							<Total 
+								resultExpenses={this.state.resultExpenses} 
+								resultIncome={this.state.resultIncome} 
+								totalBalance={this.state.totalBalance} 
+							/>
+							<History transactions={this.state.transactions}/>
+							<Operation
+							  addTransaction={this.addTransaction} 
+								addAmount={this.addAmount} 
+								addDescription={this.addDescription} 
+								description={this.state.description} 
+								amount={this.state.amount} 
+							/>            
+					</div>
+				</main>
+			</>
+		);
+	}	
 }
 
 export default App;
