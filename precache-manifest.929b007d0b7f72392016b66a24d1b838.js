2329c4eb6ebdaac51fd7d8ed2718c9d9c4831cd4
danko.anton <danko.anton@gmail.com>
2020-10-08 16:43:27 +0300

    Updates

---
+++ precache-manifest.929b007d0b7f72392016b66a24d1b838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b650b109d88dacc4cf423ac4371e61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f53a6d6932b2643a8321",
+    "url": "/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "a400c74ca57d6f9875a9",
+    "url": "/static/js/2.27a74b0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27a74b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53a6d6932b2643a8321",
+    "url": "/static/js/main.f3bc3035.chunk.js"
+  },
+  {
+    "revision": "d15900e1b3fa0397a418",
+    "url": "/static/js/runtime-main.044dbcfb.js"
+  }
+]);
